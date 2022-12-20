4780e8c7861b71de5285042ffb0a587f24167c58
SanketMajge <121082018+SanketMajge@users.noreply.github.com>
2022-12-20 12:05:43 +0530

    Update spark c

--- spark.c
+++ spark.c
@@ -2,4 +2,16 @@
 main()
 {
 printf("\n welcome");
+  printf("\n spark institute");
+  add();
 }
+void add()
+{
+  int a,b,c;
+  printf("\n enter a&b");
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  printf("\n add=%d",c);
+ 
+}
+
